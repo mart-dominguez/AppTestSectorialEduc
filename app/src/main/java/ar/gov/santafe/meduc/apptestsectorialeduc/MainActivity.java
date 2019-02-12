8f203bc5a4c0730211858f53df5cc3d4c460df83
mdomingu <mdomingu@gmailcom>
2019-02-12 10:30:42 -0300

    recibo mensajes de firebase

--- app/src/main/java/ar/gov/santafe/meduc/apptestsectorialeduc/MainActivity.java
+++ app/src/main/java/ar/gov/santafe/meduc/apptestsectorialeduc/MainActivity.java
@@ -1,13 +1,26 @@
 package ar.gov.santafe.meduc.apptestsectorialeduc;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn;
+    private TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        txt = (TextView) findViewById(R.id.textoPrincipal);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("Millis actual "+ System.currentTimeMillis());
+            }
+        });
     }
 }
